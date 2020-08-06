7576b528f207f92b59d08114d553fe5d7c27d4a7
team5 <team5@example.com>
2020-08-06 13:14:31 +0900

    update state after posting stock information

--- public_html/js/post-stock.js
+++ public_html/js/post-stock.js
@@ -1,19 +1,28 @@
-const postStock = (id, is_sold) => {
+const setState = (id, is_sold) => {
+  const state = is_sold ? 'sold' : 'non-sold';
+  const els = document.getElementById(id)
+      .querySelectorAll('.sold, .non-sold');
+  els.forEach(el => {
+    el.classList.remove('sold');
+    el.classList.remove('non-sold');
+    el.classList.add(state);
+  });
+};
+
+const isSold = (id) => {
+  const el = document.getElementById(id)
+      .querySelector('.sold, .non-sold');
+  return el.classList.contains('sold');
+};
+
+const postStock = (id) => {
   console.log(id);
   const params = new URLSearchParams();
   params.append('menu_id', id);
-  params.append('is_sold', is_sold);
+  params.append('is_sold', !isSold(id));
   axios.post('/team5/api/daily/set-stock.php', params)
-  .then(response => {
-    console.log(response);
-    const state = response.is_sold ? 'sold' : 'non-sold';
-    const els = document.getElementById(id)
-        .querySelectorAll('.sold, .non-sold');
-    console.log(els);
-    els.forEach(el => {
-      el.classList.remove('sold');
-      el.classList.remove('non-sold');
-      el.classList.add(state);
+    .then(response => {
+      console.log(response);
+      setState(id, response.data.is_sold);
     });
-  });
 };
