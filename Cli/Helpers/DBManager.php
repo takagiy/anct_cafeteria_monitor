3843feb653b987a9ebdcd9499aaa05474d39c878
team5 <team5@example.com>
2020-08-11 14:30:53 +0900

    code format

--- Cli/Helpers/DBManager.php
+++ Cli/Helpers/DBManager.php
@@ -5,19 +5,22 @@ namespace Cli\Helpers;
 use App\Models\Menu;
 use Carbon\Carbon;
 
-class DBManager {
-  public function updateDate() {
-    $oldest = Carbon::parse(Menu::min('date'));
-    $delta = Carbon::now()->startOfWeek()->diffInDays($oldest, false);
+class DBManager
+{
+    public function updateDate()
+    {
+        $oldest = Carbon::parse(Menu::min('date'));
+        $delta = Carbon::now()->startOfWeek()->diffInDays($oldest, false);
 
-    $menus = Menu::all();
-    foreach($menus as $menu) {
-      $menu->date = Carbon::parse($menu->date)->addDays($delta)->format('y-m-d');
-      $menu->save();
+        $menus = Menu::all();
+        foreach ($menus as $menu) {
+            $menu->date = Carbon::parse($menu->date)->addDays($delta)->format('y-m-d');
+            $menu->save();
+        }
     }
-  }
 
-  public function resetStocks() {
-    Menu::query()->update(['sold' => false]);
-  }  
+    public function resetStocks()
+    {
+        Menu::query()->update(['sold' => false]);
+    }
 }
