b2ad8ace71c1b8bf74ef502b4b20338c0b414739
team5 <team5@example.com>
2020-08-11 12:34:44 +0900

    cli: fix date update

--- Cli/Helpers/DBManager.php
+++ Cli/Helpers/DBManager.php
@@ -7,8 +7,8 @@ use Carbon\Carbon;
 
 class DBManager {
   public function updateDate() {
-    $oldest = Menu::min('date');
-    $delta = Carbon::now()->diffInDays(Carbon::parse($oldest));
+    $oldest = Carbon::parse(Menu::min('date'));
+    $delta = Carbon::now()->startOfWeek()->diffInDays($oldest, false);
 
     $menus = Menu::all();
     foreach($menus as $menu) {
